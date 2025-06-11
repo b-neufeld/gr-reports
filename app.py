9a3f4149e2a1eae7fc0bc0f6e6ff3c10c2d10bb2
Brahm Neufeld <neufeld@gmail.com>
2025-06-11 09:17:13 -0600

    "hello world"

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return PlainTextResponse("hello world")
