28b46d6a8c97e1fee5e32444fb5d897633905aac
Brahm Neufeld <neufeld@gmail.com>
2025-06-11 09:41:06 -0600

    Pass Goodreads RSS URL via env variable

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-# app.py
+import os
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
+GOODREADS_RSS_URL = os.getenv("GOODREADS_RSS_URL")
+
 @app.get("/")
 def read_root():
-    return PlainTextResponse("hello world")
+    if GOODREADS_RSS_URL:
+        return PlainTextResponse(f"RSS URL is: {GOODREADS_RSS_URL}")
+    else:
+        return PlainTextResponse("No RSS URL provided.")
